f55bd9f3ac5c8e31eeabde9ae69deaffa2f28724
lucasscattolin <scatto.lucas@gmail.com>
2022-08-28 12:51:53 -0300

    primer commit

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const ControlPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto,
+  gastos,
+  setGastos,
+}) => {
+  const [porcentaje, setPorcentaje] = useState(0);
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+
+    const totalDisponible = presupuesto - totalGastado;
+
+    // Calculando porcentaje para CircularProgressbar
+    const nuevoPorcentaje = (
+      ((presupuesto - totalDisponible) / presupuesto) *
+      100
+    ).toFixed(2);
+
+    setDisponible(totalDisponible);
+    setGastado(totalGastado);
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje);
+    }, 800);
+  }, [gastos]);
+
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  const handleResetApp = () => {
+    const resultado = window.confirm(
+      "¿Quieres reiniciar el presupuesto y tus gastos?"
+    );
+
+    if (resultado) {
+      setGastos([]);
+      setIsValidPresupuesto(false);
+      setPresupuesto(0);
+    }
+  };
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: porcentaje > 100 ? "#DC2626" : "#3B82F6",
+            trailColor: "#F5F5F5",
+            textColor: porcentaje > 100 ? "#DC2626" : "#3B82F6",
+          })}
+          value={porcentaje}
+          text={`${porcentaje}% Gastado`}
+        />
+      </div>
+      <div className="contenido-presupuesto">
+        <button type="button" className="reset-app" onClick={handleResetApp}>
+          Resetear App
+        </button>
+        <p>
+          <span>Presupuesto: </span>
+          {formatearCantidad(presupuesto)}
+        </p>
+
+        <p className={`${disponible < 0 ? "negativo" : ""}`}>
+          <span>Disponible: </span>
+          {formatearCantidad(disponible)}
+        </p>
+        <p>
+          <span>Gastado: </span>
+          {formatearCantidad(gastado)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ControlPresupuesto;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from "react";
+import Mensaje from "./Mensaje";
+import CerrarBtn from "../img/cerrar.svg";
+
+const Modal = ({
+  setModal,
+  animarModal,
+  setAnimarModal,
+  guardarGasto,
+  gastoEditar,
+  setGastoEditar,
+}) => {
+  const [mensaje, setMensaje] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [cantidad, setCantidad] = useState(0);
+  const [categoria, setCategoria] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    if (Object.keys(gastoEditar).length > 0) {
+      setNombre(gastoEditar.nombre);
+      setCantidad(gastoEditar.cantidad);
+      setCategoria(gastoEditar.categoria);
+      setFecha(gastoEditar.fecha);
+      setId(gastoEditar.id);
+    }
+  }, []);
+
+  const ocultarModal = () => {
+    setAnimarModal(false);
+    setGastoEditar({});
+
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([nombre, cantidad, categoria].includes("")) {
+      setMensaje("Todos los campos son obligatorios");
+
+      setTimeout(() => {
+        setMensaje("");
+      }, 3000);
+
+      return;
+    }
+
+    guardarGasto({ nombre, cantidad, categoria, fecha, id });
+  };
+
+  return (
+    <div className="modal">
+      <div className="cerrar-modal">
+        <img
+          src={CerrarBtn}
+          alt="Boton de cerrar modal"
+          onClick={ocultarModal}
+        />
+      </div>
+
+      <form
+        className={`formulario ${animarModal ? "animar" : "cerrar"}`}
+        onSubmit={handleSubmit}
+      >
+        <legend>{gastoEditar.nombre ? "Editar Gasto" : "Nuevo gasto"}</legend>
+
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre del Gasto</label>
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el nombre del gasto..."
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade cantidad gastada..."
+            value={cantidad}
+            onChange={(e) => setCantidad(Number(e.target.value))}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="categoria">Categoría</label>
+          <select
+            id="categoria"
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
+          >
+            <option value="">-- --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+            <option value="varios">Varios</option>
+          </select>
+        </div>
+
+        <input
+          type="submit"
+          value={gastoEditar.nombre ? "Guardar Cambios" : "Añadir Gasto"}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Modal;
